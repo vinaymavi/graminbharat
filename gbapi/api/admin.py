e34132d9c91fc1c45134484f9c4baadbda528b45
vinay mavi <vinaymavi@gmail.com>
2019-01-06 14:52:25 +0530

    Admin site name change

--- gbapi/api/admin.py
+++ gbapi/api/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+
 from .models import State, PlanUnit, District, Block, Village,Report
 
-
+# Change admin site header
+AdminSite.site_header = 'Gramin Bhart Admin Panel'
+AdminSite.site_title = 'Gramin Bhart Admin Panel'
 admin.site.register(PlanUnit)
 admin.site.register(State)
 admin.site.register(District)
